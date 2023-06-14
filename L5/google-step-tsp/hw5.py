723dab21b7bdf66f05b031a4facbd5c94d61cc0b
rshimaok <rihs14804@gmail.com>
2023-06-14 12:47:15 +0900

    greedy + 2-opt

--- L5/google-step-tsp/hw5.py
+++ L5/google-step-tsp/hw5.py
@@ -1,7 +1,11 @@
 import sys
 import random
 import numpy as np
+from math import sqrt
 from common import print_tour, read_input
+
+def dist(a,b):
+    return sqrt(pow(a[0] - b[0], 2.0) + pow(a[1] - b[1], 2.0))
 
 def greedy(cities, start, N):
     # Save index of cities
@@ -11,26 +15,38 @@ def greedy(cities, start, N):
     distance = 0
     while unvisited:
         current = tour[-1]
-        current_pos = np.array([cities[current][0], cities[current][1]])
-        next_city= min(unvisited, key=lambda city: np.linalg.norm(current_pos - np.array([cities[city][0], cities[city][1]])))
-        distance += np.linalg.norm(current_pos - np.array([cities[next_city][0], cities[next_city][1]]))
+        next_city= min(unvisited, key=lambda city: dist(cities[current], cities[city]))
+        distance += dist(cities[current], cities[next_city])
         tour.append(next_city)
         unvisited.remove(next_city)
-    distance += np.linalg.norm(np.array([cities[start][0], cities[start][1]]) - np.array([cities[tour[-1]][0], cities[tour[-1]][1]]))
-    return tour, distance
+    return tour
+
+def two_opt(cities,tour,N):
+    for _ in range(100):
+        a = random.randint(0,N-3)
+        b = random.randint(a+1,N-1)
+        before = dist(cities[tour[a]], cities[tour[a+1]]) + dist(cities[tour[b]], cities[tour[(b+1)%N]])
+        after = dist(cities[tour[a]], cities[tour[b]]) + dist(cities[tour[a+1]], cities[tour[(b+1)%N]])
+        if before > after:
+            tour[a+1:b+1] = reversed(tour[a+1:b+1])
+    return sum(dist(cities[tour[i]],cities[tour[i + 1]]) for i in range(N-1)) + dist(cities[tour[0]],cities[tour[N-1]])
 
 # for i in `seq 0 6`;do python3 hw5.py input_${i}.csv > output_${i}.csv; done
+# python3 hw5.py input_1.csv
 # python3 -m http.server
 # http://localhost:8000/visualizer/build/default/index.html
 if __name__ == '__main__':
     assert len(sys.argv) > 1
-    N = len(read_input(sys.argv[1]))
+    cities = read_input(sys.argv[1])
+    N = len(cities)
     min_dist = float('inf')
-    for i in range(10):
-        start = random.randint(0, N-1)
-        ans = greedy(read_input(sys.argv[1]),start,N)
-        if ans[1] < min_dist:
-            min_dist = ans[1]
-            tour = ans[0]
-            print(min_dist,tour)
-    print_tour(tour)
+    for i in range(N):
+        start = i
+        tour = greedy(cities,start,N)
+        # print(start,tour)
+        two_opt_dist = two_opt(cities,tour,N)
+        if two_opt_dist < min_dist:
+            min_dist = two_opt_dist
+            ans = tour
+    # print(ans,min_dist)
+    print_tour(ans)
